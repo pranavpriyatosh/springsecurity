bbcb15a197c32a13d9d1230664f1ae07bedc872a
pranavpriyatosh <pranav.priyatosh@intelliswift.com>
2023-05-01 10:15:03 +0530

    refactor

--- section2_custombasic/basiccustomization/src/test/java/com/intelli/SimpleBankBackendApplicationTests.java
+++ section2_custombasic/basiccustomization/src/test/java/com/intelli/SimpleBankBackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EazyBankBackendApplicationTests {
+class SimpleBankBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
