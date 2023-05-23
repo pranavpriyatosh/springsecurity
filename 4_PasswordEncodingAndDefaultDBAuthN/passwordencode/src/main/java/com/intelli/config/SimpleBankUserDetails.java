319401b31536839e851196617a1c4282bf2705ff
pranavpriyatosh <pranav.priyatosh@intelliswift.com>
2023-05-23 06:20:40 +0530

    mysql DB and refactor

--- 4_PasswordEncodingAndDefaultDBAuthN/passwordencode/src/main/java/com/intelli/config/SimpleBankUserDetails.java
+++ 4_PasswordEncodingAndDefaultDBAuthN/passwordencode/src/main/java/com/intelli/config/SimpleBankUserDetails.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class EazyBankUserDetails  implements UserDetailsService {
+public class SimpleBankUserDetails  implements UserDetailsService {
 
     @Autowired
     private CustomerRepository customerRepository;
